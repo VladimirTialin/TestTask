e00b5ae90fc2995c81d69340f5c24613b649a1d0
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-07-03 23:10:28 +0300

    Функеция ввода размера массива

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿int getFromUserArraySize(int count = 3)
+{
+    int size;
+    string inputSize = "Введите размер массива: ";
+    Console.Write(inputSize);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    if (int.TryParse(Console.ReadLine(), out size) && size > 0 && size < int.MaxValue)
+    {
+        Console.ResetColor();
+        return size;
+    }
+    else if (count != 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Ошибка ввода! Вы попытались указать размер массива в виде текста, символа, отрицательного числа,\nлибо введенное число привело к переполнению типа ");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("int (целочисленного типа данных)!");
+        Console.ResetColor();
+        Console.WriteLine($"Повторите ввод. Количество попыток - {count}");
+        return getFromUserArraySize(count - 1);
+    }
+    else
+    {
+        size = new Random().Next(1, 9);
+        Console.WriteLine($"Размер массива [{size}] - сформирован автоматически!");
+    }
+    Console.ResetColor();
+    return size;
+}
